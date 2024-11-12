c2139b311eaccb39e24ea640941a997d287e4ff9
giolga <giolga.kumi@gmail.com>
2024-11-12 19:09:46 +0400

    Add project files.

---
+++ Mafia-Razor-Pages/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mafia_Razor_Pages.Models
+{
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public byte? Age { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Gmail { get; set; }
+    }
+}
