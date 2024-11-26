e12104d5ba8959da4a6e967cddd94e5548f90d56
el kumi <giolga.kumi@gmail.com>
2024-11-26 18:19:30 +0400

    character Selection page added

---
+++ Mafia-Razor-Pages/Pages/Selection.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mafia_Razor_Pages.Pages
+{
+    public class SelectionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
